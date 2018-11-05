29dbf0e0249a8bc686738886e913439b0b33f457
chen_anguo <cag2050@163.com>
2018-11-05 17:31:58 +0800

    m

--- src/fs/链式流_文件解压缩.js
+++ src/fs/链式流_文件解压缩.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const zlib = require('zlib')
 
 let readStream = fs.createReadStream('zip.txt.gz')
-let zip = zlib.createGunzip()
+let unzip = zlib.createGunzip()
 let writeStream = fs.createWriteStream('unzip.txt')
 
 readStream
-    .pipe(zip)
+    .pipe(unzip)
     .pipe(writeStream)
 
-console.log('文件压缩完成');
+console.log('文件解压缩完成');
