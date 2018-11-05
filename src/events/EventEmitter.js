ab1048c651d8983ec1db73b156e524a5c84b5990
chen_anguo <cag2050@163.com>
2018-11-05 12:05:01 +0800

    m

--- src/events/EventEmitter.js
+++ src/events/EventEmitter.js
@@ -5,7 +5,9 @@ class MyEmitter extends EventEmitter {
 }
 
 let myEmitter = new MyEmitter()
-myEmitter.on('test', () => {
+myEmitter.on('test', (value) => {
     console.log('test事件被触发');
+    console.log(`传入的值为：${value}`);
 })
-myEmitter.emit('test')
+myEmitter.emit('test', 'test1')
+myEmitter.emit('test', 'test2')
